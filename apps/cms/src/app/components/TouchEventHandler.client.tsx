1871575f8a6145b1fd2a7456aa66b8de34444cca
johntips <tu.ue.et@gmail.com>
2025-01-30 13:12:56 +0900

    fix: metadata api from react-helmet

---
+++ apps/cms/src/app/components/TouchEventHandler.client.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { useEffect } from 'react';
+
+export function TouchEventHandler() {
+  useEffect(() => {
+    const handleTouchMove = (event: TouchEvent) => {
+      if (event.touches.length > 1) {
+        event.preventDefault();
+      }
+    };
+
+    document.addEventListener('touchmove', handleTouchMove, { passive: false });
+
+    return () => {
+      document.removeEventListener('touchmove', handleTouchMove);
+    };
+  }, []);
+
+  return null;
+} 
